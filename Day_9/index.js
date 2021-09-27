b9f60356ec8eeae552172031491b9e9e9e858b74
HarshDhoble <harshdhoble26@gmail.com>
2021-09-27 22:47:58 +0530

    Tha 9 complete

---
+++ Day_9/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+console.log(__dirname);
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine", "jade");
+app.use('/',(req,res) => {
+    /*Sendfile sends file to front end and path.join is used to append the current directory to the specified path*/ 
+    //res.sendFile(path.join(__dirname,"public/images/Doffy.jpg"));
+   // res.download
+    //res.download(path.join(__dirname,"public/images/Straw_Hats.png"),"download.jpg");
+   res.render('index',{title:"Express"})
+   res.status(201).cookie("toke","test", {
+       expire: new Date(Date.now()+ 8*3600000)
+   })
+   //.cookie("hello","hello")
+   //.redirect(301, "/admin")
+})
+
+app.listen(3000);
